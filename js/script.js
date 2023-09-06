69b5f92b0d9839e0ac6231d2780d131328073fe8
Dorota Maryniak <docia209aa@gmail.com>
2023-09-06 21:26:30 +0200

    Add player move

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ else if (randomNumber == 3) {
   	computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
